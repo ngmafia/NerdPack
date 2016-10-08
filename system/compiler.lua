a7fe6d781505a1729190b14052d80f876bbaf4b1
Jaime Marques <mrthesoulz@gmail.com>
2016-10-08 20:27:52 +0100

    lets get some parsing

--- system/compiler.lua
+++ system/compiler.lua
@@ -3,58 +3,58 @@ local _, NeP = ...
 NeP.Compiler = {}
 
 local spellTokens = {
-  {'actions', '^%%'},
-  {'along', '^&'},
-  {'lib', '^@'},
-  {'macro', '^/'}
+	{'actions', '^%%'},
+	{'along', '^&'},
+	{'lib', '^@'},
+	{'macro', '^/'}
 }
 
 -- Takes a string a produces a table in its place
 function NeP.Compiler:Spell(eval)
-  local ref = {
-    spell = eval[1]
-  }
-  if ref.spell:find('^!') then
-    ref.interrupts = true
-    ref.spell = ref.spell:sub(2)
-  end
-  for i=1, #spellTokens do
-    local kind, patern = unpack(spellTokens[i])
-    if ref.spell:find(patern) then
-      ref.spell = ref.spell:sub(2)
-      ref.token = kind
-    end
-  end
-  ref.spell = NeP.Spells:Convert(ref.spell)
-  eval[1] = ref
+	local ref = {
+		spell = eval[1]
+	}
+	if ref.spell:find('^!') then
+		ref.interrupts = true
+		ref.spell = ref.spell:sub(2)
+	end
+	for i=1, #spellTokens do
+		local kind, patern = unpack(spellTokens[i])
+		if ref.spell:find(patern) then
+			ref.spell = ref.spell:sub(2)
+			ref.token = kind
+		end
+	end
+	ref.spell = NeP.Spells:Convert(ref.spell)
+	eval[1] = ref
 end
 
 function NeP.Compiler:Target(eval)
-  local ref = {
-    target = ref
-  }
-  if ref.target:find('.ground') then
-    ref.target = ref.target:sub(0,-8)
-    ref.ground = true
-  end
-  eval[3] = ref
+	local ref = {
+		target = ref
+	}
+	if ref.target:find('.ground') then
+		ref.target = ref.target:sub(0,-8)
+		ref.ground = true
+	end
+	eval[3] = ref
 end
 
 function NeP.Compiler:Iterate(eval)
-  local spell, cond, target = unpack(eval)
-  -- Take care of spell
-  if type(spell) == 'table' then
-    self:Iterate(spell)
-  elseif type(spell) == 'string' then
-    self:Spell(eval)
-  elseif type(spell) == 'function' then
-    eval[1] = {
-      spell = spell,
-      token = 'func'
-    }
-  end
-  -- Take care of target
-  if type(target) == 'string' then
-    self:Target(eval)
-  end
+	local spell, cond, target = unpack(eval)
+	-- Take care of spell
+	if type(spell) == 'table' then
+		self:Iterate(spell)
+	elseif type(spell) == 'string' then
+		self:Spell(eval)
+	elseif type(spell) == 'function' then
+		eval[1] = {
+			spell = spell,
+			token = 'func'
+		}
+	end
+	-- Take care of target
+	if type(target) == 'string' then
+		self:Target(eval)
+	end
 end
--- system/parser.lua
+++ system/parser.lua
@@ -22,28 +22,28 @@ local function castingTime()
 end
 
 local fake_target = {
-  func = function()
-    return UnitExists('target') and 'target' or 'player'
-  end
+	func = function()
+		return UnitExists('target') and 'target' or 'player'
+	end
 }
 
 function NeP.Parser:Target(eval)
-  -- This is so we dont generate garbage when using fake_target
+	-- This is so we dont generate garbage when using fake_target
 	eval = eval or fake_target
-  if eval.func then
-    eval.target = eval.func()
-  end
-  -- Check unit
-  if UnitExists(eval.target) and UnitIsVisible(eval.target)
-	and Engine.LineOfSight('player', eval.target) then
-    return true
-  end
+	if eval.func then
+		eval.target = eval.func()
+	end
+	-- Check unit
+	if UnitExists(eval.target) and UnitIsVisible(eval.target) then
+		return true
+	end
 end
 
 function NeP.Parser:Spell(eval)
-  if eval.tokens then
-    --TODO, actions like &, @ or function
-  end
+	print(eval.spell)
+	if eval.token then
+		print('hit')
+	end
 	local skillType = GetSpellBookItemInfo(eval.spell)
 	local isUsable, notEnoughMana = IsUsableSpell(eval.spell)
 	if skillType ~= 'FUTURESPELL' and isUsable and not notEnoughMana then
@@ -55,34 +55,34 @@ function NeP.Parser:Spell(eval)
 end
 
 function NeP.Parser:Parse(eval)
-  local spell, cond, target = eval
-  if type(spell) == 'table' then
-    --if conditions_pass then
-      local r = self:Parse(spell)
-      if r then return true end
-    --end
-  elseif spell.interrupts or spell.token or (castingTime('player') == 0) then
-    if self:Target(target) then
-      if self:Spell(spell) --[[AND conditions_pass]]then
-        -- Cancels the current cast
-        if spell.interrupts then
-          SpellStopCasting()
-        end
-        -- TODO, EXECUTE/CAST
-        return true
-      end
-    end
-  end
+	if not eval[1].spell then
+		if NeP.DSL:Parse(eval[2]) then
+			local r = self:Parse(eval[1])
+			if r then return true end
+		end
+	elseif eval[1].interrupts or eval[1].token or (castingTime('player') == 0) then
+		if self:Target(eval[3]) then
+			if self:Spell(eval[1]) and NeP.DSL:Parse(eval[2]) then
+				print('final')
+				-- Cancels the current cast
+				if eval[1].interrupts then
+					SpellStopCasting()
+				end
+				-- TODO, EXECUTE/CAST
+				return true
+			end
+		end
+	end
 end
 
 C_Timer.NewTicker(0.1, (function()
 	if NeP.DSL:Get('toggle')(nil, 'mastertoggle') then
-        if not UnitIsDeadOrGhost('player') and IsMountedCheck() then
-          local table = NeP.CR.CR[InCombatLockdown()]
-          for i=1, #table do
-            local r = NeP.Parser:Parse(table[i])
-            if r then break end
-          end
-        end
+		if not UnitIsDeadOrGhost('player') and IsMountedCheck() then
+			local table = NeP.CR.CR[InCombatLockdown()]
+			for i=1, #table do
+				local r = NeP.Parser:Parse(table[i])
+				if r then break end
+			end
+		end
 	end
 end), nil)
