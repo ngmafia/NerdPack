788e86e75ba363eeb696f58f1226dae938f6a2ec
Jaime Marques <mrthesoulz@gmail.com>
2016-10-13 01:48:20 +0100

    add NeP:Queue(SPELL, TARGET)

--- system/queuer.lua
+++ system/queuer.lua
@@ -3,20 +3,30 @@ local _, NeP = ...
 NeP.Queuer = {}
 local Queue = {}
 
-function NeP.Queuer:Add(_spell, _target)
-	print(_spell)
-	if not _spell then return end
-	Queue[_spell] = {
-		spell = _spell,
-		nil,
-		target = _target or 'target'
-	}
+function NeP.Queuer:Add(spell, target)
+	spell = NeP.Spells:Convert(spell)
+	if not spell then return end
+	Queue[spell] = target or UnitExists('target') and 'target' or 'player'
+end
+
+function NeP.Queuer:Spell(spell)
+ 	local skillType = GetSpellBookItemInfo(spell)
+	local isUsable, notEnoughMana = IsUsableSpell(spell)
+	if skillType ~= 'FUTURESPELL' and isUsable and not notEnoughMana then
+		local GCD = NeP.DSL:Get('gcd')()
+		if GetSpellCooldown(spell) <= GCD then
+			return true
+		end
+	end
 end
 
 function NeP.Queuer:Execute()
-	for spell, table in pairs(Queue) do
-		local r = NeP.Parser:Parse(Queue[spell])
-		if r then return true end
+	for spell, target in pairs(Queue) do
+		if self:Spell(spell) then
+			NeP.Protected.Cast(spell, target)
+			Queue[spell] = nil
+			return true
+		end
 	end
 end
 
