d4b710f64d42cb9fc73544c473242c46b1f8aaef
Jaime Marques <mrthesoulz@gmail.com>
2016-10-13 22:24:21 +0100

    fix libs

--- system/dsl.lua
+++ system/dsl.lua
@@ -26,7 +26,7 @@ local OPs = {
 	['/'] = function(arg1, arg2) return arg1 / arg2 end,
 	['*'] = function(arg1, arg2) return arg1 * arg2 end,
 	['!'] = function(arg1, arg2) return not DSL.Parse(arg1, arg2) end,
-	['@'] = function(arg1, arg2) return NeP.library.parse(arg1) end,
+	['@'] = function(arg1, arg2) return NeP.Library:Parse(arg1) end,
 	['true'] = function() return true end,
 	['false'] = function() return false end,
 }
