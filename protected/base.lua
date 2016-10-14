3b5bfc8b5d450921089f525cb0324781fe0009fa
Jaime Marques <mrthesoulz@gmail.com>
2016-10-14 21:21:51 +0100

    re-add nameplates

--- protected/base.lua
+++ protected/base.lua
@@ -1,4 +1,4 @@
-local _, NeP = ...
+local n_name, NeP = ...
 
 NeP.Protected.Cast = function(spell, target)
 	NeP.Faceroll:Set(spell, target)
@@ -55,4 +55,23 @@ NeP.OM.Maker = function()
 		local object = ValidUnits[i]
 		NeP.OM:Add(object)
 	end
+end
+
+-- Dont load this for 7.1 or up
+local version = GetBuildInfo()
+if not version:find('^7.0') then return end
+
+local lnr = LibStub("AceAddon-3.0"):NewAddon(n_name, "LibNameplateRegistry-1.0")
+	
+function lnr:OnEnable()
+	self:LNR_RegisterCallback("LNR_ON_NEW_PLATE")
+	self:LNR_RegisterCallback("LNR_ON_RECYCLE_PLATE")
+end
+
+function lnr:LNR_ON_NEW_PLATE(_, _, plateData)
+	NeP.OM:Add(plateData.unitToken)
+end
+
+function lnr:LNR_ON_RECYCLE_PLATE()
+	NeP.OM:Garbage()
 end
