2abc7a9d4714ebf40d1db377e4094974a9fc76d8
Jaime Marques <mrthesoulz@gmail.com>
2016-10-15 18:38:30 +0100

    fix fakeunits

--- fakeunits.lua
+++ fakeunits.lua
@@ -11,6 +11,7 @@ NeP.FakeUnits:Add('lowest', function(num, role)
 			}
 		end
 	end
+	table.sort( tempTable, function(a,b) return a.prio > b.prio end )
 	return tempTable[num] and tempTable[num].key
 end)
 
@@ -25,5 +26,6 @@ NeP.FakeUnits:Add('tank', function(num)
 			}
 		end
 	end
+	table.sort( tempTable, function(a,b) return a.prio > b.prio end )
 	return tempTable[num] and tempTable[num].key
 end)
