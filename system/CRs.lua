7ba4dc0e67ab00ee52de8b7daa3c7eb80f2362f4
Jaime Marques <mrthesoulz@gmail.com>
2016-10-15 01:50:37 +0100

    CR wiper

--- system/CRs.lua
+++ system/CRs.lua
@@ -18,6 +18,16 @@ function NeP.CR:AddGUI(key, eval)
 	NeP.Interface:AddCR_ST(key)
 end
 
+function NeP.CR:Wipe(eval)
+	for i=1,#eval do
+		if type(eval[i]) == 'table' then
+			self:Wipe(eval[i])
+		end
+		eval[i] = nil
+	end
+	eval = nil
+end
+
 function NeP.CR:Add(SpecID, Name, InCombat, OutCombat, ExeOnLoad, GUI)
 	local classIndex = select(3, UnitClass('player'))
 	if NeP.ClassTable[classIndex][SpecID] or classIndex == SpecID then
@@ -39,6 +49,8 @@ function NeP.CR:Add(SpecID, Name, InCombat, OutCombat, ExeOnLoad, GUI)
 		CRs[SpecID][Name][false] = OutCombat
 	else
 		-- Unload every cr not needed
+		NeP.CR:Wipe(InCombat)
+		NeP.CR:Wipe(OutCombat)
 		SpecID, Name, InCombat, OutCombat, ExeOnLoad, GUI = nil
 	end
 end
