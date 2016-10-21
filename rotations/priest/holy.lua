ec08017221d67f118c9efb8382aafc5e645728b0
Yobleed <freelauncher1@hotmail.com>
2016-10-21 11:01:07 +0200

    Some UI additions (#121)
    * Some UI additions
    
    * Fix 
    
    thanks xD

--- rotations/priest/holy.lua
+++ rotations/priest/holy.lua
@@ -1,12 +1,19 @@
 local GUI = {
+	--------------------------------------------Cooldowns------------------------------------------------
+	----------------------------------------------------------------------------------------------------
+	{type = 'header', 	text = 'Cooldowns when toggled on', align = 'center'},
+	{type = 'checkbox', text = 'Use Guardian Spirit', key = 'c_GS', default = false},
+	{type = 'spinner', text = '', key = 'sc_GS', default = 20},
+	                      	{type = 'ruler'},{type = 'spacer'},
 	
 	--------------------------------------------TRINKETS------------------------------------------------
 	----------------------------------------------------------------------------------------------------
 	{type = 'header', 	text = 'Trinkets', align = 'center'},
+	{type = 'checkbox', text = 'Use Trinkets', key = 'u_T', default = false},
 	{type = 'text', 	text = 'Check to Enable Trinkets', align = 'center'},
 	{type = 'checkbox', text = 'Top Trinket enabled', key = 'trinket_1', default = false},
 	{type = 'checkbox', text = 'Bottom Trinket enabled', key = 'Trinket_2', default = false},
-	
+	                      	{type = 'ruler'},{type = 'spacer'},
 	--------------------------------------------KEYBINDS-----------------------------------------------------
 	---------------------------------------------------------------------------------------------------
 	
@@ -15,6 +22,7 @@ local GUI = {
 	{type = 'checkbox', text = 'Holy Word: Sanctify enabled', key = 'k_HWS', default = false},
 	{type = 'checkbox', text = 'Mass Dispel enabled', key = 'k_MD', default = false},
 	{type = 'checkbox', text = 'Pause enabled', key = 'k_P', default = false},
+	                        {type = 'ruler'},{type = 'spacer'},
 
 	--------------------------------------------POTIONS-------------------------------------------------
 	----------------------------------------------------------------------------------------------------
@@ -27,6 +35,7 @@ local GUI = {
 	{type = 'spinner', text = 'Ancient Healing Potion', key = 'sp_AHP', default = 20},
 	{type = 'checkbox', text = 'Ancient Mana Potion', key = 'p_AMP', default = false},
 	{type = 'spinner', text = 'Ancient Mana Potion', key = 'sp_AMP', default = 10},
+	                         {type = 'ruler'},{type = 'spacer'},
 	
 	--------------------------------------------DPS-----------------------------------------------------
 	----------------------------------------------------------------------------------------------------
@@ -34,13 +43,59 @@ local GUI = {
 	{type = 'text', 	text = 'Check to enable extra DPS', align = 'center'},
 	{type = 'checkbox', text = 'Holy Word: Chastise enabled', key = 'd_HWC', default = false},
 	{type = 'checkbox', text = 'Holy Fire enabled', key = 'd_HF', default = false},
-
+	                         {type = 'ruler'},{type = 'spacer'},
 	
+----------------------------------------------------TANK-------------------------------------------------
+	----------------------------------------------------------------------------------------------------
+
+	{type = 'header', 	text = 'Tank', align = 'center'},
+	{type = 'text', 	text = 'Tank health values', align = 'center'},
+	{type = 'spinner', text = 'Holy Word: Serenity', key = 't_HWSE', default = 60},
+	{type = 'spinner', text = 'Flash Heal', key = 't_FH', default = 70},
+	{type = 'spinner', text = 'Prayer of Mending', key = 't_PoM', default = 100},
+	{type = 'spinner', text = 'Renew', key = 't_Ren', default = 100},
+	                         {type = 'ruler'},{type = 'spacer'},
+
+--------------------------------------------------PLAYER-----------------------------------------------------
+	----------------------------------------------------------------------------------------------------
+
+	{type = 'header', 	text = 'Player', align = 'center'},
+	{type = 'text', 	text = 'Player health values', align = 'center'},
+	{type = 'spinner', text = 'Gift of the Naaru', key = 'p_Gift', default = 20},
+	{type = 'spinner', text = 'Holy Word: Serenity', key = 'p_HWSE', default = 60},
+	{type = 'spinner', text = 'Flash Heal', key = 'p_FH', default = 70},
+	{type = 'spinner', text = 'Prayer of Mending', key = 'p_PoM', default = 100},
+	{type = 'spinner', text = 'Renew', key = 'p_Ren', default = 100},
+	                         {type = 'ruler'},{type = 'spacer'},	
+	
+--------------------------------------------------LOWEST-----------------------------------------------------
+	----------------------------------------------------------------------------------------------------
+
+	{type = 'header', 	text = 'Lowest', align = 'center'},
+	{type = 'text', 	text = 'Lowest health values', align = 'center'},
+	{type = 'spinner', text = 'Holy Word: Serenity', key = 'l_HWSE', default = 60},
+	{type = 'spinner', text = 'Flash Heal', key = 'l_FH', default = 70},
+	{type = 'spinner', text = 'Prayer of Mending', key = 'l_PoM', default = 100},
+	{type = 'spinner', text = 'Renew', key = 'l_Ren', default = 80},
+	{type = 'spinner', text = 'Heal', key = 'l_H', default = 90},
+	                         {type = 'ruler'},{type = 'spacer'},	
+	
+	-------------------------------------------MOVING-----------------------------------------------------
+	----------------------------------------------------------------------------------------------------
+
+	{type = 'header', 	text = 'Moving', align = 'center'},
+	{type = 'text', 	text = 'Movement speed', align = 'center'},
+	{type = 'checkbox', text = 'Angelic Feather enabled', key = 'm_AF', default = false},
+	{type = 'text', 	text = 'Lowest health and moving values', align = 'center'},
+	{type = 'spinner', text = 'Holy Word: Serenity', key = 'm_HWSE', default = 60},
+	{type = 'spinner', text = 'Flash Heal Surge of Light', key = 'm_FH', default = 70},
+	{type = 'spinner', text = 'Renew', key = 'm_Ren', default = 80},
+	                         {type = 'ruler'},{type = 'spacer'},	
 }
 
 local Cooldowns = {
-	--Guardian Spirit if below or if lowest health is 20% if Guardian Angel Talent selected.
-	{'!Guardian Spirit', 'lowest.health <= 20 & talent(4,2)', 'lowest'}
+	--Guardian Spirit if below or if lowest health is below or if UI value and checked.
+	{'!Guardian Spirit', 'lowest.health <= UI(sc_GS) & UI(c_GS)', 'lowest'}
 }	
 
 local Trinkets = {
@@ -51,11 +106,11 @@ local Trinkets = {
 }
 
 local Keybinds = {
-	--Mass Dispel on Mouseover target Left Control.
+	--Mass Dispel on Mouseover target Left Control when checked in UI.
 	{'Mass Dispel', 'keybind(lctrl) & UI(k_MD)', 'mouseover.ground'},											 
-	--Holy Word: Sanctify on Mouseover target left shift.
+	--Holy Word: Sanctify on Mouseover target left shift when checked in UI.
 	{'Holy Word: Sanctify', 'keybind(lshift) & UI(k_HWS)', 'mouseover.ground'},												 
-	-- Pause on left alt.
+	-- Pause on left alt when checked in UI.
 	{'%pause', 'keybind(lalt)& UI(k_P)'}																														 
 }
 
@@ -76,47 +131,40 @@ local SpiritOfRedemption = {
 }
 			
 local DPS = {
-	--Holy Word: Chastise on cooldown if not healing .
+	--Holy Word: Chastise on cooldown if not healing when checked in UI.
 	{'Holy Word: Chastise', 'UI(d_HWC)' , 'target'},																
-	--Holy Fire on cooldown if not healing.
+	--Holy Fire on cooldown if not healing when checked in UI.
 	{'Holy Fire', 'UI(d_HF)' , 'target'},																					 
 	--Smite on cooldown if not healing.
 	{'Smite', nil, 'target'}
 }
 
-local FastHeals = {
-	--Holy Word: Serenity on cooldown if lowest health is below 30%.
-	{'Holy Word: Serenity', nil, 'lowest'},
-	--Flash Heal on cooldown if lowest health is below 30%.
-	{'Flash Heal', nil, 'lowest'}
-}
-
 local Tank = {
-	--Holy Word: Serenity if tank health below or if 60%.
-	{'Holy Word: Serenity', 'tank.health <= 60', 'tank'},
-	--Flash heal if tank health below or if 70%.
-	{'Flash Heal', 'tank.health <= 70', 'tank'},
-	--Prayer of Mending if tank missing Prayer of Mending.
-	{'Prayer of Mending', '!tank.buff(Prayer of Mending)', 'tank'},
-	--Renew if tank missing Renew.
-	{'Renew', '!tank.buff(Renew)', 'tank'}
+	--Holy Word: Serenity if tank health is below or if UI value.
+	{'Holy Word: Serenity', 'tank.health <= UI(t_HWSE)', 'tank'},
+	--Flash heal if tank health is below or if UI value.
+	{'Flash Heal', 'tank.health <= UI(t_FH)', 'tank'},
+	--Prayer of Mending if tank missing Prayer of Mending and when tank health is below or if UI value.
+	{'Prayer of Mending', '!tank.buff(Prayer of Mending) & tank.health <= UI(t_PoM)', 'tank'},
+	--Renew if tank missing Renew and when tank health is below or if UI value.
+	{'Renew', '!tank.buff(Renew) & tank.health <= UI(t_Ren)', 'tank'}
 	
 }
 
 local Player = {
-	--Gift of the Naaru if player health is below or if 20%.
-	{'Gift of the Naaru', 'player.health <= 20', 'player'},
-	--Holy Word: Serenity if player health is below or if 60%.
-	{'Holy Word: Serenity', 'player.health <= 60', 'player'},
-	--Flash Heal if player health is below or if 60%.
-	{'Flash Heal', 'player.health <= 60', 'player'},
-	--Prayer of Mending if player missing Prayer of Mending.
-	{'Prayer of Mending', '!player.buff(Prayer of Mending)', 'player'},
-	--Renew if player missing Renew.
-	{'Renew', '!player.buff(Renew)', 'player'},
-	--Prayer of Healing if player and 4 or more others at 20yds are 65% health ore below.
+	--Gift of the Naaru if player health is below or if UI value.
+	{'Gift of the Naaru', 'player.health <= UI(p_Gift)', 'player'},
+	--Holy Word: Serenity if player health is below or if UI value.
+	{'Holy Word: Serenity', 'player.health <= UI(p_HWSE)', 'player'},
+	--Flash Heal if player health is below or if UI value.
+	{'Flash Heal', 'player.health <= UI(p_FH)', 'player'},
+	--Prayer of Mending if player missing Prayer of Mending and player health is below or if UI value.
+	{'Prayer of Mending', '!player.buff(Prayer of Mending) & player.health <= UI(p_PoM)', 'player'},
+	--Renew if player missing Renew and player health is below or if UI value.
+	{'Renew', '!player.buff(Renew) & player.health <= UI(p_Ren)', 'player'},
+	--Prayer of Healing if player and 4 or more others at 20yds are below or if 65% health
 	{'Prayer of Healing', 'player.area(20, 65).heal >= 4 ', 'lowest'},
-	--Holy Nova if player and 4 or more others at 10yds are 90% health or below.
+	--Holy Nova if player and 4 or more others at 10yds are below or if 90% health.
 	{'Holy Nova', 'player.area(10, 90).heal >= 4', 'player'},
 	
 }
@@ -126,31 +174,33 @@ local Lowest = {
 	{'Flash Heal', 'player.buff(Surge of Light) & player.buff(Surge of Light).duration <= 3 & lowest.health < 100', 'lowest'},
 	--Gift of the Naaru if lowest health is below or if 20% and has Guardian Spirit.
 	{'Gift of the Naaru', 'lowest.health <= 20 & lowest.buff(Guardian Spirit)', 'lowest'},
-	--Holy Word: Serenity if lowest health is below or if 60%.
-	{'Holy Word: Serenity', 'lowest.health <= 60', 'lowest'},
-	--Flash Heal if lowest health is below or if 70%.			
-	{'Flash Heal', 'lowest.health <= 70', 'lowest'},
-	--Prayer of Healing if lowest and 4 or more others are 65% health or below.
+	--Holy Word: Serenity if lowest health is below or if UI value.
+	{'Holy Word: Serenity', 'lowest.health <= UI(l_HWSE)', 'lowest'},
+	--Flash Heal if lowest health is below or if UI value.		
+	{'Flash Heal', 'lowest.health <= UI(l_FH)', 'lowest'},
+	--Prayer of Healing if lowest and 4 or more others are below or if 65% health
 	{'Prayer of Healing', 'lowest.area(20, 65).heal >= 4' , 'lowest'},
-	--Prayer of Mending if lowest health is missing Prayer of Mending.
-	{'Prayer of Mending', '!lowest.buff(Prayer of Mending)', 'lowest'},
-	--Renew if lowest health is missing Renew.
-	{'Renew', '!lowest.buff(Renew)', 'lowest'},
-	--Heal if Lowest Healt is below or if 90%.
-	{'Heal', 'lowest.health <= 90', 'lowest'}
+	--Prayer of Mending if lowest health missing Prayer of Mending and is below or if UI value.
+	{'Prayer of Mending', '!lowest.buff(Prayer of Mending) & lowest.health <= UI(l_PoM)', 'lowest'},
+	--Renew if lowest health is missing Renew and is below or if UI value.
+	{'Renew', '!lowest.buff(Renew) & lowest.health <= UI(l_Ren)', 'lowest'},
+	--Heal if Lowest Healt is below or if UI value.
+	{'Heal', 'lowest.health <= UI(l_H)', 'lowest'}
 }
 
 local Moving = {
-	--Holy Word: Serenity if lowest health below or if 60% and moving.
-	{'Holy Word: Serenity', 'lowest.health <= 60', 'lowest'},
-	--Renew if lowest health is missing Renew and Lowest health below 100% and moving.
-	{'Renew', '!lowest.buff(Renew) & lowest.health < 100', 'lowest'},
+	--Gift of the Naaru if lowest health is below or if 20% and has Guardian Spirit.
+	{'Gift of the Naaru', 'lowest.health <= 20 & lowest.buff(Guardian Spirit)', 'lowest'},
+	--Holy Word: Serenity if lowest health  is below or if UI value.
+	{'Holy Word: Serenity', 'lowest.health <= UI(m_HWSE)', 'lowest'},
+	--Renew if lowest health is missing Renew and Lowest health is below or if UI value.
+	{'Renew', '!lowest.buff(Renew) & lowest.health <= UI(m_Ren)', 'lowest'},
 	--Flash Heal charge Dump if Surge of Light duration is less or equal to 3 seconds and moving
 	{'Flash Heal', 'player.buff(Surge of Light) & player.buff(Surge of Light).duration <= 3 & lowest.health < 100', 'lowest'},
-	--Flash Heal when Surge of Light is active, Lowest Health is below 70% and moving.
-	{'Flash Heal', 'player.buff(Surge of Light) & lowest.health < 70', 'lowest'},
-	--Angelic Feather if player is moving for 2 seconds or longer and Missing Angelic Feather and if UI enables it. WIP
-	{'Angelic Feather', 'player.movingfor >= 2 & !player.buff(Angelic Feather) & spell(Angelic Feather).charges >= 1', 'player.ground'} --UI enable
+	--Flash Heal when Surge of Light is active, Lowest Health  is below or if UI value.
+	{'Flash Heal', 'player.buff(Surge of Light) & lowest.health <= UI(m_FH)', 'lowest'},
+	--Angelic Feather if player is moving for 2 seconds or longer and Missing Angelic Feather and if UI enables it. 
+	{'Angelic Feather', 'player.movingfor >= 2 & !player.buff(Angelic Feather) & spell(Angelic Feather).charges >= 1 & UI(m_AF)', 'player.ground'} 
 	
 }
 
@@ -158,13 +208,12 @@ local inCombat = {
 	--Fade when you get aggro.
 	{'fade', 'aggro'},
 	{Cooldowns, 'toggle(cooldowns)'},	
-	{Trinkets},
+	{Trinkets, 'UI(u_T)'},
 	{Keybinds},
 	{Potions},
 	{SpiritOfRedemption, 'player.buff(Spirit of Redemption)'},
 	{'%dispelall'},
 	{{
-		{FastHeals, 'lowest.health < 30'},
 		{Tank, 'tank.health < 100'},
 		{Player, 'health < 100'},
 		{Lowest, 'lowest.health < 100'},
