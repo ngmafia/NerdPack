c543a50382977845755076faf8c919bf4628860f
Jaime Marques <mrthesoulz@gmail.com>
2016-10-25 08:14:28 +0100

    Add mouse(KEY) and local stuff for performance

--- conditions/keybinds.lua
+++ conditions/keybinds.lua
@@ -1,25 +1,39 @@
 local _, NeP = ...
 
+-- Locals to increase performance
+local IsShiftKeyDown          = IsShiftKeyDown
+local IsLeftShiftKeyDown      = IsLeftShiftKeyDown
+local IsRightShiftKeyDown     = IsRightShiftKeyDown
+local IsControlKeyDown        = IsControlKeyDown
+local IsLeftControlKeyDown    = IsLeftControlKeyDown
+local IsRightControlKeyDown   = IsRightControlKeyDown
+local IsAltKeyDown            = IsAltKeyDown
+local IsLeftAltKeyDown        = IsLeftAltKeyDown
+local IsRightAltKeyDown       = IsRightAltKeyDown
+local IsMouseButtonDown       = IsMouseButtonDown
+local GetCurrentKeyBoardFocus = GetCurrentKeyBoardFocus
+
 local KEYBINDS = {
 	-- Shift
-	['shift'] = function() return IsShiftKeyDown() end,
-	['lshift'] = function() return IsLeftShiftKeyDown() end,
-	['rshift'] = function() return IsRightShiftKeyDown() end,
+	['shift']    = function() return IsShiftKeyDown() end,
+	['lshift']   = function() return IsLeftShiftKeyDown() end,
+	['rshift']   = function() return IsRightShiftKeyDown() end,
 	-- Control
-	['control'] = function() return IsControlKeyDown() end,
+	['control']  = function() return IsControlKeyDown() end,
 	['lcontrol'] = function() return IsLeftControlKeyDown() end,
 	['rcontrol'] = function() return IsRightControlKeyDown() end,
 	-- Alt
-	['alt'] = function() return IsAltKeyDown() end,
-	['lalt'] = function() return IsLeftAltKeyDown() end,
-	['ralt'] = function() return IsRightAltKeyDown() end,
-	-- Mouse
-	['mouse3'] = function() return IsMouseButtonDown(3) end,
-	['mouse4'] = function() return IsMouseButtonDown(4) end,
-	['mouse5'] = function() return IsMouseButtonDown(5) end,
+	['alt']      = function() return IsAltKeyDown() end,
+	['lalt']     = function() return IsLeftAltKeyDown() end,
+	['ralt']     = function() return IsRightAltKeyDown() end,
 }
 
 NeP.DSL:Register("keybind", function(_, Arg)
 	Arg = Arg:lower()
 	return KEYBINDS[Arg] and KEYBINDS[Arg]() and not GetCurrentKeyBoardFocus()
+end)
+
+NeP.DSL:Register("mouse", function(_, Arg)
+	Arg = tonumber(Arg:lower())
+	return KEYBINDS[Arg] and IsMouseButtonDown(Arg) and not GetCurrentKeyBoardFocus()
 end)
