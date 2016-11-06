4131fd66a991e93806602f60ef497c106d650520
Jaime Marques <mrthesoulz@gmail.com>
2016-11-06 18:15:19 +0000

    fix test

--- conditions/general.lua
+++ conditions/general.lua
@@ -55,6 +55,8 @@ local GetAverageItemLevel         = GetAverageItemLevel
 local UnitIsCharmed               = UnitIsCharmed
 local IsSwimming                  = IsSwimming
 local IsFalling                   = IsFalling
+local UnitBuff                    = UnitBuff
+local UnitDebuff                  = UnitDebuff
 
 local SPELL_POWER_INSANITY       = SPELL_POWER_INSANITY
 local SPELL_POWER_FOCUS          = SPELL_POWER_FOCUS
@@ -606,13 +608,13 @@ end)
 ------------------------------------------ BUFFS/FUNCs -----------------------------------
 ------------------------------------------------------------------------------------------
 
-local function UnitBuff(target, spell, own)
+local function UnitBuffL(target, spell, own)
 	local spell, rank = GetSpellInfo(spell)
 	local name,_,_,count,_,_,expires,caster = UnitBuff(target, spell, rank or '', own and 'PLAYER')
 	return name, count, expires, caster
 end
 
-local function UnitDebuff(target, spell, own)
+local function UnitDebuffL(target, spell, own)
 	local spell, rank = GetSpellInfo(spell)
 	local name, _,_, count, _,_, expires, caster = UnitDebuff(target, spell, rank or '', own and 'PLAYER')
 	return name, count, expires, caster
@@ -622,7 +624,7 @@ local heroismBuffs = { 32182, 90355, 80353, 2825, 146555 }
 NeP.DSL:Register("hashero", function()
 	for i = 1, #heroismBuffs do
 		local SpellName = NeP.Core:GetSpellName(heroismBuffs[i])
-		local buff = UnitBuff('player', SpellName)
+		local buff = UnitBuffL('player', SpellName)
 		if buff then return true end
 	end
 	return false
@@ -631,20 +633,20 @@ end)
 ------------------------------------------ BUFFS -----------------------------------------
 ------------------------------------------------------------------------------------------
 NeP.DSL:Register("buff", function(target, spell)
-	return not not UnitBuff(target, spell, true)
+	return not not UnitBuffL(target, spell, true)
 end)
 
 NeP.DSL:Register("buff.any", function(target, spell)
-	return not not UnitBuff(target, spell)
+	return not not UnitBuffL(target, spell)
 end)
 
 NeP.DSL:Register("buff.count", function(target, spell)
-	local buff, count = UnitBuff(target, spell, true)
+	local buff, count = UnitBuffL(target, spell, true)
 	return not not buff and count or 0
 end)
 
 NeP.DSL:Register("buff.duration", function(target, spell)
-	local buff,_,expires = UnitBuff(target, spell, true)
+	local buff,_,expires = UnitBuffL(target, spell, true)
 	return buff and (expires - GetTime()) or 0
 end)
 
@@ -652,20 +654,20 @@ end)
 ------------------------------------------------------------------------------------------
 
 NeP.DSL:Register("debuff", function(target, spell)
-	return not not UnitDebuff(target, spell, true)
+	return not not UnitDebuffL(target, spell, true)
 end)
 
 NeP.DSL:Register("debuff.any", function(target, spell)
-	return not not UnitDebuff(target, spell)
+	return not not UnitDebuffL(target, spell)
 end)
 
 NeP.DSL:Register("debuff.count", function(target, spell)
-	local debuff,count = UnitDebuff(target, spell, true)
+	local debuff,count = UnitDebuffL(target, spell, true)
 	return not not debuff and count or 0
 end)
 
 NeP.DSL:Register("debuff.duration", function(target, spell)
-	local debuff,_,expires = UnitDebuff(target, spell)
+	local debuff,_,expires = UnitDebuffL(target, spell)
 	return debuff and (expires - GetTime()) or 0
 end)
 
