90b74908200809f057eba0a4bc97e63c685195ee
Jaime Marques <mrthesoulz@gmail.com>
2017-06-17 17:02:11 +0100

    if scanning

--- system/tooltip.lua
+++ system/tooltip.lua
@@ -3,6 +3,8 @@ local _, NeP = ...
 NeP.Tooltip = {}
 NeP.Globals.Tooltip = NeP.Tooltip
 
+local UIParent = UIParent
+local UnitName = UnitName
 local frame = CreateFrame('GameTooltip', 'NeP_ScanningTooltip', UIParent, 'GameTooltipTemplate')
 
 local function pPattern(text, pattern)
@@ -17,27 +19,27 @@ local function pPattern(text, pattern)
 	end
 end
 
-function NeP.Tooltip:Scan_Buff(target, pattern)
+function NeP.Tooltip.Scan_Buff(_, target, pattern)
 	for i = 1, 40 do
 		frame:SetOwner(UIParent, 'ANCHOR_NONE')
 		frame:SetUnitBuff(target, i)
 		local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
-		return tooltipText and pPattern(tooltipText, pattern)
+		if tooltipText and pPattern(tooltipText, pattern) then return end
 	end
 	return false
 end
 
-function NeP.Tooltip:Scan_Debuff(target, pattern)
+function NeP.Tooltip.Scan_Debuff(_, target, pattern)
 	for i = 1, 40 do
 		frame:SetOwner(UIParent, 'ANCHOR_NONE')
 		frame:SetUnitDebuff(target, i)
 		local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
-		return tooltipText and pPattern(tooltipText, pattern)
+		if tooltipText and pPattern(tooltipText, pattern) then return true end
 	end
 	return false
 end
 
-function NeP.Tooltip:Unit(target, pattern)
+function NeP.Tooltip.Unit(_, target, pattern)
 	frame:SetOwner(UIParent, 'ANCHOR_NONE')
 	frame:SetUnit(target)
 	local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
@@ -45,7 +47,7 @@ function NeP.Tooltip:Unit(target, pattern)
 	return tooltipText and pPattern(tooltipText, pattern)
 end
 
-function NeP.Tooltip:Tick_Time(target)
+function NeP.Tooltip.Tick_Time(_, target)
 	frame:SetOwner(UIParent, 'ANCHOR_NONE')
 	frame:SetUnitBuff(target)
 	local tooltipText = _G["NeP_ScanningTooltipTextLeft2"]:GetText()
