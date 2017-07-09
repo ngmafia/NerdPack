c0da779e81dec5453572562ff50e441e926d73a2
Jaime Marques <mrthesoulz@gmail.com>
2017-07-10 00:15:10 +0100

    Remove deprecated

--- system/conditions.lua
+++ system/conditions.lua
@@ -1,23 +1,12 @@
 local _, NeP = ...
 
 NeP.DSL = {}
-
 local conditions = {}
-
-local Deprecated_Warn = {}
-local function Deprecated(Strg)
-	if Deprecated_Warn[Strg] then
-		NeP.Core:Print(Strg..' Was deprecated, use: '..Deprecated_Warn[Strg].replace..'instead.')
-		Deprecated_Warn[Strg] = nil
-	end
-end
-
 local noop = function() end
 
 function NeP.DSL.Get(_, Strg)
 	Strg = Strg:lower()
 	if conditions[Strg] then
-		Deprecated(Strg)
 		return conditions[Strg]
 	end
 	return noop
@@ -39,14 +28,5 @@ function NeP.DSL.Register(_, name, condition, overwrite)
 		_add(name, condition, overwrite)
 	else
 		NeP.Core:Print("ERROR! tried to add an invalid condition")
-	end
-end
-
-function NeP.DSL:Register_Deprecated(name, replace, condition, overwrite)
-	name = name:lower()
-	self:Register(name, condition, overwrite)
-	if not Deprecated_Warn[name] then
-		Deprecated_Warn[name] = {}
-		Deprecated_Warn[name].replace = replace
 	end
 end
