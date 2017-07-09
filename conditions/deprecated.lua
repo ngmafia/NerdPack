36941720d63118aa05d25fb2d7920dddcd538d1b
Jaime Marques <mrthesoulz@gmail.com>
2017-07-10 00:15:52 +0100

    Remove deprecated

--- conditions/deprecated.lua
+++ conditions/deprecated.lua
@@ -1,9 +1,9 @@
 local _, NeP = ...
 
-NeP.DSL:Register_Deprecated("isself", "is", function(target)
+NeP.DSL:Register("isself", "is", function(target)
   return NeP.DSL:Get("fury.diff")(target, 'player')
 end)
 
-NeP.DSL:Register_Deprecated('furydiff', 'fury.diff', function(target)
+NeP.DSL:Register('furydiff', 'fury.diff', function(target)
   return NeP.DSL:Get("fury.diff")(target)
 end)
