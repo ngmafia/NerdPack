d20f4cdfd1b5bb906975cc3ea0d68d8cd0964822
Jaime Marques <mrthesoulz@gmail.com>
2017-07-23 14:20:04 +0100

    remove the cache from unit.area

--- conditions/area.lua
+++ conditions/area.lua
@@ -4,67 +4,50 @@ local UnitGUID = UnitGUID
 local C_Timer = C_Timer
 local wipe = wipe
 
-local Cache = {
-  Enemy              = {},
-  Enemy_infront      = {},
-  Friendly           = {},
-  Friendly_infront   = {},
-}
-
-C_Timer.NewTicker(1, (function()
-  for tb in pairs(Cache) do
-    wipe(Cache[tb])
-  end
-end), nil)
-
-local function testunit(ounit, unit, distance, bypass, infront)
-  local unit_dist = NeP.Protected.Distance(ounit, unit)
-  if (NeP.DSL:Get('combat')(unit) or bypass)
-    and unit_dist <= tonumber(distance)
-    and (not infront or NeP.Protected.Infront(ounit, unit)) then
-    return true
-  end
-end
-
-local function iterate(tb, unit, distance, infront)
-  -- Cache
-  local oGUID = UnitGUID(unit)
-  local ct = infront and tb..'_infront' or tb
-  if Cache[ct][oGUID] then
-    return Cache[ct][oGUID]
-  end
+-- USAGE: UNIT.area(DISTANCE).enemies >= #
+NeP.DSL:Register("area.enemies", function(unit, distance)
   local total = 0
-  -- OM
-  for _, Obj in pairs(NeP.OM:Get(tb, true)) do
-    if testunit(unit, Obj.key, distance, Obj.isdummy or tb ~= 'Enemy', infront) then
+  for _, Obj in pairs(NeP.OM:Get('Enemy', true)) do
+    if NeP.DSL:Get('combat')(unit) or Obj.isdummy
+    and NeP.Protected.Distance(unit, Obj.key) < distance then
       total = total +1
     end
   end
-  -- cache the result for this unit
-  Cache[ct][oGUID] = total
   return total
-end
-
--- USAGE: UNIT.aoe(DISTANCE).enemies >= #
-NeP.DSL:Register("area.enemies", function(unit, distance)
-  if not UnitExists(unit) then return 0 end
-  return iterate('Enemy', unit, distance)
 end)
 
--- USAGE: UNIT.aoe(DISTANCE).enemies.infront >= #
+-- USAGE: UNIT.area(DISTANCE).enemies.infront >= #
 NeP.DSL:Register("area.enemies.infront", function(unit, distance)
-  if not UnitExists(unit) then return 0 end
-  return iterate('Enemy', unit, distance, true)
+  local total = 0
+  for _, Obj in pairs(NeP.OM:Get('Enemy', true)) do
+    if NeP.DSL:Get('combat')(unit) or Obj.isdummy
+    and NeP.Protected.Distance(unit, Obj.key) < distance
+    and NeP.Protected.Infront(unit, Obj.key) then
+      total = total +1
+    end
+  end
+  return total
 end)
 
--- USAGE: UNIT.aoe(DISTANCE).friendly >= #
+-- USAGE: UNIT.area(DISTANCE).friendly >= #
 NeP.DSL:Register("area.friendly", function(unit, distance)
-  if not UnitExists(unit) then return 0 end
-  return iterate('Friendly', unit, distance)
+  local total = 0
+  for _, Obj in pairs(NeP.OM:Get('Friendly', true)) do
+    if NeP.Protected.Distance(unit, Obj.key) < distance then
+      total = total +1
+    end
+  end
+  return total
 end)
 
--- USAGE: UNIT.aoe(DISTANCE).friendly >= #
+-- USAGE: UNIT.area(DISTANCE).friendly.infront >= #
 NeP.DSL:Register("area.friendly.infront", function(unit, distance)
-  if not UnitExists(unit) then return 0 end
-  return iterate('Friendly', unit, distance, true)
+  local total = 0
+  for _, Obj in pairs(NeP.OM:Get('Friendly', true)) do
+    if NeP.Protected.Distance(unit, Obj.key) < distance
+    and NeP.Protected.Infront(unit, Obj.key) then
+      total = total +1
+    end
+  end
+  return total
 end)
