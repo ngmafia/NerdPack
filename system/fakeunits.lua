d788797cd41fcf1d16750c03e904ccced155a478
Jaime Marques <mrthesoulz@gmail.com>
2017-07-23 14:51:58 +0100

    revert

--- system/fakeunits.lua
+++ system/fakeunits.lua
@@ -24,8 +24,10 @@ end
 
 -- /dump NeP.FakeUnits:Filter("lowest")
 local function process(unit)
+	local arg = unit:match('%((.+)%)')
+	local num = tonumber(unit:match("%d+") or 1)
 	unit = unit:gsub('%((.+)%)', ''):gsub("%d+", '')
-	return Units[unit] and Units[unit](tonumber(unit:match("%d+") or 1), unit:match('%((.+)%)')) or unit
+	return Units[unit] and Units[unit](num, arg) or unit
 end
 
 local function not_in_tbl(unit, tbl)
