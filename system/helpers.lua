07ee5fdc5facdc3107635f8a5c3a2dc30bc5f9a3
Jaime Marques <mrthesoulz@gmail.com>
2017-07-23 10:54:30 +0100

    Lower blacklist time from 0.5 to 0.1

--- system/helpers.lua
+++ system/helpers.lua
@@ -27,14 +27,14 @@ end
 
 local function blackListSpell(GUID, spell)
 	_Failed[GUID][spell] =  true
-	C_Timer.After(0.5, (function()
+	C_Timer.After(0.1, (function()
 		_Failed[GUID][spell] =  nil
 	end), nil)
 end
 
 local function blackListInfront(GUID)
 	_Failed[GUID].infront = true
-	C_Timer.After(0.5, (function()
+	C_Timer.After(0.1, (function()
 		_Failed[GUID].infront = nil
 	end), nil)
 end
