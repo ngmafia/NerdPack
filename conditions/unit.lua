6e6604e5d789f9d1ce6186107287670ae575195e
Jaime Marques <mrthesoulz@gmail.com>
2017-07-29 12:11:41 +0100

    add distancefrom fix range

--- conditions/unit.lua
+++ conditions/unit.lua
@@ -182,10 +182,12 @@ NeP.DSL:Register('enemy', function(target)
   return UnitExists(target) and UnitCanAttack('player', target)
 end)
 
--- usage: UNIT1.distance(UNIT2) > #
--- UNIT2 is optional and fallsback to player if not provided
-NeP.DSL:Register({'distance', 'range'}, function(unit1, unit2)
-  return NeP.Protected.UnitCombatRange(unit2 or 'player', unit1)
+NeP.DSL:Register({'distance', 'range'}, function(unit)
+  return NeP.Protected.UnitCombatRange('player', unit)
+end)
+
+NeP.DSL:Register({'distancefrom', 'rangefrom'}, function(unit, unit2)
+  return NeP.Protected.UnitCombatRange(unit2, unit)
 end)
 
 NeP.DSL:Register('level', function(target)
