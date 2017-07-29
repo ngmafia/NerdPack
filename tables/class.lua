2f6f7c11976db0beefb8cbc7ed96ed6b1bb0a643
Jaime Marques <mrthesoulz@gmail.com>
2017-07-29 13:23:34 +0100

    export NeP.ClassTable

--- tables/class.lua
+++ tables/class.lua
@@ -1,22 +1,27 @@
 local _, NeP = ...
 
+local rangex = {
+	["melee"] = 1.5,
+	["ranged"] = 40,
+}
+
 NeP.ClassTable = {
 	{ -- Warrior
 		class = 'Warrior',
 		hex = 'c79c6e',
 		rgb = {0.78,0.61,0.43},
 		[71] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Arms',
 			role = 'DPS'
 		},
 		[72] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Fury',
 			role = 'DPS'
 		},
 		[73] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Protection',
 			role = 'TANK'
 		}
@@ -26,17 +31,17 @@ NeP.ClassTable = {
 		hex = 'f58cba',
 		rgb = {0.96,0.55,0.73},
 		[65] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Holy',
 			role = 'HEALER'
 		},
 		[66] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Protection',
 			role = 'TANK'
 		},
 		[70] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Retribution',
 			role = 'DPS'
 		}
@@ -46,17 +51,17 @@ NeP.ClassTable = {
 		hex = 'abd473',
 		rgb = {0.67,0.83,0.45},
 		[253] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Beast Mastery',
 			role = 'DPS'
 		},
 		[254] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Marksmanship',
 			role = 'DPS'
 		},
 		[255] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Survival',
 			role = 'DPS'
 		}
@@ -66,17 +71,17 @@ NeP.ClassTable = {
 		hex = 'fff569',
 		rgb = {1,0.96,0.41},
 		[259] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Assassination',
 			role = 'DPS'
 		},
 		[260] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Outlaw',
 			role = 'DPS'
 		},
 		[261] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Subtlety',
 			role = 'DPS'
 		}
@@ -86,17 +91,17 @@ NeP.ClassTable = {
 		hex = 'ffffff',
 		rgb = {1,1,1},
 		[256] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Discipline',
 			role = 'HEALER'
 		},
 		[257] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Holy',
 			role = 'HEALER'
 		},
 		[258] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Shadow',
 			role = 'HEALER'
 		}
@@ -106,17 +111,17 @@ NeP.ClassTable = {
 		hex = 'c41f3b',
 		rgb = {0.77,0.12,0.23},
 		[250] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Blood',
 			role = 'TANK'
 		},
 		[251] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Frost',
 			role = 'DPS'
 		},
 		[252] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Unholy',
 			role = 'DPS'
 		}
@@ -126,17 +131,17 @@ NeP.ClassTable = {
 		hex = '0070de',
 		rgb = {0,0.44,0.87},
 		[262] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Elemental',
 			role = 'DPS'
 		},
 		[263] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Enhancement',
 			role = 'DPS'
 		},
 		[264] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Restoration',
 			role = 'HEALER'
 		}
@@ -146,17 +151,17 @@ NeP.ClassTable = {
 		hex = '69ccf0',
 		rgb = {0.41,0.8,0.94},
 		[62] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Arcane',
 			role = 'DPS'
 		},
 		[63] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Fire',
 			role = 'DPS'
 		},
 		[64] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Frost',
 			role = 'DPS'
 		}
@@ -166,17 +171,17 @@ NeP.ClassTable = {
 		hex = '9482c9',
 		rgb = {0.58,0.51,0.79},
 		[265] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Affliction',
 			role = 'DPS'
 		},
 		[266] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Demonology',
 			role = 'DPS'
 		},
 		[267] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Destruction',
 			role = 'DPS'
 		}
@@ -186,17 +191,17 @@ NeP.ClassTable = {
 		hex = '00ff96',
 		rgb = {0,1,0.59},
 		[268] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Brewmaster',
 			role = 'TANK'
 		},
 		[269] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Windwalker',
 			role = 'DPS'
 		},
 		[270] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Mistweaver',
 			role = 'HEALER'
 		}
@@ -206,22 +211,22 @@ NeP.ClassTable = {
 		hex = 'ff7d0a',
 		rgb = {1,0.49,0.04},
 		[102] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Balance',
 			role = 'DPS'
 		},
 		[103] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Feral Combat',
 			role = 'DPS'
 		},
 		[104] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Guardian',
 			role = 'TANK'
 		},
 		[105] = {
-			range = 'Ranged',
+			range = rangex["ranged"],
 			name = 'Restoration',
 			role = 'HEALER'
 		}
@@ -231,14 +236,16 @@ NeP.ClassTable = {
 		hex = 'A330C9',
 		rgb = {0.64,0.19,0.79},
 		[577] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Havoc',
 			role = 'DPS'
 		},
 		[581] = {
-			range = 'melee',
+			range = rangex["melee"],
 			name = 'Vengeance',
 			role = 'TANK'
 		}
 	}
 }
+
+NeP.Globals.ClassTable = NeP.ClassTable
