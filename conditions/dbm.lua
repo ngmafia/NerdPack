e950f6fae4ce4988fef90734ff940135b4d3d43d
Jaime Marques <mrthesoulz@gmail.com>
2017-07-30 13:57:42 +0100

    clean up and deprecate pull_timer in favor of dbm(pull in)

--- conditions/dbm.lua
+++ conditions/dbm.lua
@@ -4,29 +4,19 @@ local DBM = DBM
 if not DBM then return end
 
 local C_Timer = C_Timer
-local Timers = { ["Pull in"] = {timer = 999} }
+local GetSpellInfo = GetSpellInfo
+local Timers = {}
 local fake_timer = 999
 
 C_Timer.NewTicker(0.1, function()
   for bar in pairs(DBM.Bars.bars) do
-      local number = string.match(bar.id ,"%d+")
-      Timers[bar.id] = Timers[bar.id] or {}
-      Timers[bar.id].timer = bar.timer
-      Timers[bar.id].spellid = number
+      local id = GetSpellInfo(bar.id:match("%d+")) or bar.id:lower()
+      Timers[id] = bar.timer
   end
 end)
 
 NeP.DSL:Register('dbm', function(_, event)
-  local T = Timers[event]
-  if not T then return fake_timer end
-  T.timer = (T.timer > 0) and T.timer or fake_timer
-  return T.timer
-end)
-
-NeP.DSL:Register('pull_timer', function()
-  local T = Timers["Pull in"]
-  T.timer = (T.timer > 0) and T.timer or fake_timer
-  return T.timer
+  return Timers[event:lower()] or fake_timer
 end)
 
 --Export to globals
--- conditions/deprecated.lua
+++ conditions/deprecated.lua
@@ -1,9 +1,13 @@
 local _, NeP = ...
 
 NeP.DSL:Register("isself", function(target)
-  return self:Get("is")(target, 'player')
+  return NeP.DSL:Get("is")(target, 'player')
 end)
 
 NeP.DSL:Register('furydiff', function(target)
-  return self:Get("fury.diff")(target)
+  return NeP.DSL:Get("fury.diff")(target)
+end)
+
+NeP.DSL:Register('pull_timer', function()
+  return NeP.DSL:Get('dbm')(nil, "Pull in")
 end)
