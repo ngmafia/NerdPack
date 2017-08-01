aa0f588a2b0678a201f83a40a7b50bcc4f1f1e16
Jaime Marques <mrthesoulz@gmail.com>
2017-08-01 11:47:21 +0100

    unit.area must take combatreach into account

--- conditions/area.lua
+++ conditions/area.lua
@@ -7,7 +7,7 @@ NeP.DSL:Register("area.enemies", function(unit, distance)
   local total = 0
   for _, Obj in pairs(NeP.OM:Get('Enemy', true)) do
     if (NeP.DSL:Get('combat')(unit) or Obj.isdummy)
-    and NeP.Protected.Distance(unit, Obj.key) < tonumber(distance) then
+    and NeP.DSL:Get("range")(unit, Obj.key) < tonumber(distance) then
       total = total +1
     end
   end
@@ -20,7 +20,7 @@ NeP.DSL:Register("area.enemies.infront", function(unit, distance)
   local total = 0
   for _, Obj in pairs(NeP.OM:Get('Enemy', true)) do
     if (NeP.DSL:Get('combat')(unit) or Obj.isdummy)
-    and NeP.Protected.Distance(unit, Obj.key) < tonumber(distance)
+    and NeP.DSL:Get("range")(unit, Obj.key) < tonumber(distance)
     and NeP.Protected.Infront(unit, Obj.key) then
       total = total +1
     end
@@ -33,7 +33,7 @@ NeP.DSL:Register("area.friendly", function(unit, distance)
   if not UnitExists(unit) then return 0 end
   local total = 0
   for _, Obj in pairs(NeP.OM:Get('Friendly', true)) do
-    if NeP.Protected.Distance(unit, Obj.key) < tonumber(distance) then
+    if NeP.DSL:Get("range")(unit, Obj.key) < tonumber(distance) then
       total = total +1
     end
   end
@@ -45,7 +45,7 @@ NeP.DSL:Register("area.friendly.infront", function(unit, distance)
   if not UnitExists(unit) then return 0 end
   local total = 0
   for _, Obj in pairs(NeP.OM:Get('Friendly', true)) do
-    if NeP.Protected.Distance(unit, Obj.key) < tonumber(distance)
+    if NeP.DSL:Get("range")(unit, Obj.key) < tonumber(distance)
     and NeP.Protected.Infront(unit, Obj.key) then
       total = total +1
     end
